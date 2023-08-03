0196b86279de90fe61d4c853008b8d37843c16ce
Riyad <Riyad9876@gmail.com>
2023-08-03 16:08:01 -0400

    Form.js, MovieDisplay.js

---
+++ src/components/Form.js
@@ -0,0 +1,7 @@
+
+
+export default function Form(props){
+    return(
+        <h1> Form </h1>
+    )
+}
---
+++ src/components/MovieDisplay.js
@@ -0,0 +1,7 @@
+
+
+export default function MovieDisplay(props){
+    return (
+        <h1> Movie Display</h1>
+    )
+}
