6bba3c29ffeb86ff7ae91406ce3c80361d2705bd
Riyad <Riyad9876@gmail.com>
2023-08-03 16:53:46 -0400

    handleChange,  handleSubmit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import MovieDisplay from './components/MovieDisplay';
 import Form from './components/Form';
+import { useEffect, useState } from 'react';
 
 
 
 function App() {
+  const apiKey = '54cc14be'
+
+  const [movie, setMovie] = useState(null)
+
+  const getMovie = async (searchTerm) => {
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`)
+    const data = await res.json()
+    console.log(data);
+    setMovie(data)
+  }
+
   return (
-    <div className="">
-      <Form/>
+    <div className="App">
+      <Form getMovie={getMovie}/>
       <MovieDisplay/>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,29 @@
-
+import { useState } from "react"
 
 export default function Form(props){
+
+    const {getMovie} = props
+    const [formData, setFormData] = useState('')
+
+    const handleChange = (e) =>{
+        e.preventDefault()
+        setFormData(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        getMovie(formData)
+        setFormData('')
+    }
+
     return(
+        <>
         <h1> Form </h1>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Insert Movie Name" value={formData} onChange={handleChange}/>
+            <button>Search</button>
+
+        </form>
+        </>
     )
 }
